7c3509084303bd520fd167858754c0a007161912
fedisaev <127963492+Fed0102@users.noreply.github.com>
2024-05-13 15:59:52 +0300

    add dev server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {Random} from "./test";
 
 console.log(Random(5, 5));
+
+document.body.innerHTML = `<div style="color: red">HELLO</div>`
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,27 @@
 import path from "path";
 import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/buildConfig/buildWebpackConfig";
-import {BuildPaths} from "./config/buildConfig/types/config";
+import {BuildEnv, BuildPaths} from "./config/buildConfig/types/config";
 
-const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-}
+export default (env: BuildEnv) => {
 
-const mode = 'development';
-const isDev = mode === 'development';
-const config: webpack.Configuration = buildWebpackConfig({
-    mode: 'development',
-    paths,
-    isDev
-})
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
 
-export default config;
+    const mode = env.mode || 'development';
+    const PORT = env.port || 3000;
+
+    const isDev = mode === 'development';
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT,
+    })
+
+    return config;
+};
