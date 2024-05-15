6546a4e4d5eba74e8779323c191be0095370de19
fedisaev <127963492+Fed0102@users.noreply.github.com>
2024-05-15 15:03:28 +0300

    add styles + theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './index.scss';
+import './styles/index.scss';
 import {Routes, Route, Link} from "react-router-dom";
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {toggleTheme, theme} = useTheme()
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to='/'>Main page</Link>
             <Link to='/about'>About page</Link>
             <Suspense fallback={<div>Loading...</div>}>
